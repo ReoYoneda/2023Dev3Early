ebd1e03693e2c5f64d184bd34585f5c3e4c1b4a7
masayoshi <2101138@s.asojuku.ac.jp>
2023-05-23 14:58:41 +0900

    hen

--- 30_実装/Create.sql
+++ 30_実装/Create.sql
@@ -10,6 +10,7 @@ CREATE TABLE evaluation
 
 CREATE TABLE users
 (user_id INT AUTO_INCREMENT,
+ user_loginid VARCHAR(128) NOT NULL,
  user_password VARCHAR(128) NOT NULL,
  user_name VARCHAR(128) NOT NULL,
  user_course VARCHAR(128) NOT NULL,
