f2de00d187d7bf1197d1e091745a6cf0992bfab0
masayoshi <2101138@s.asojuku.ac.jp>
2023-05-26 16:18:28 +0900

    test

--- 30_実装/test01.php
+++ 30_実装/test01.php
@@ -1,4 +1,18 @@
-<?php
-$n = 5;
-echo $n*$n;
-?>
+<!DOCTYPE html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>webstore</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style_test.css">
+</head>
+<body>
+    <div class="animation-box"></div>
+    <div class="animation-multi"></div>
+</body>
+</html>
---
+++ 30_実装/test02.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>webstore</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style_test.css">
+</head>
+<body>
+    <div class="container" style="background-color: #F0F0F0;">
+            
+        <div class="row justify-content-center">
+        
+            <div style="width: 100%; background-color:#FFDFFF">laksdfj</div>
+            <div class="text-center" style="width: 100%; background-color:#DFFFFF">laksdfj</div>
+
+            <div class="pt-2 pb-5
+                        col-sm-10 col-md-8 col-lg-6 col-xl-5"
+                 style="background-color:#FFFFFF">
+
+                <div class="row mt-3">
+                    <span>ログインID</span>
+                    <input type="text" name="loginID" placeholder="loginID">
+                </div>
+
+                <div class="row mt-3">
+                    <span>パスワード</span>
+                    <input type="password" name="password" placeholder="password">
+                </div>
+
+                <div class="row mt-3">
+                    <input type="submit" value="ログイン">
+                </div>
+
+                <div class="row justify-content-end mt-3">
+                    <p></p>
+                    <div class="col-6 text-right">
+                        <input type="submit" value="ログイン">
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
